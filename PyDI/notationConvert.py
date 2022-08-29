5d9adf09f723694ccaa912e6051ab2ed65de1f9a
Tyler Batkin <111218991+dippio@users.noreply.github.com>
2022-08-30 00:13:31 +1000

    minor fixes
    forgot to ctrl-s on a file before uploading :sob:

--- PyDI/notationConvert.py
+++ PyDI/notationConvert.py
@@ -1,3 +1,7 @@
 # pydi v0.1
 # simple cli program for playing midi instruments in the terminal
 # notationConvert.py // functions for converting mido data into standard music notation
+
+chromaticFlats = ["C", "C♯", "D♭", "D", "D♯", "E♭", "E", "F", "F♯", "G♭", "G", "G♯", "A♭", "A", "A♯", "B♭", "B"]         #chromatic scale
+chromaticScale = ["C", "C♯", "D", "D♯", "E", "F", "F♯", "G", "G♯", "A", "A♯", "B"]                                      #chromatic scale without flats
+
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# PyDI - Python MIDI device player
-## A simple cli program for playing midi instruments in the terminal
+# PyDI - A simple cli program for playing MIDI instruments in the terminal
+
+# Installation
+Run 'pip install -r requirements.txt' 
+
+# Usage
+run 'main.py'
